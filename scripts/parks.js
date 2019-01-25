ae44fbe7a2dded9bae4b70ffc30559dab0e29577
Kirren Covey <kirrencovey@gmail.com>
2019-01-25 14:52:24 -0600

    fetch now accepts user input

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,42 +1,59 @@
+let userInputParks;
+let address;
 
-let userInputParks = "dog_park";
 
-// Fetch all parks data
+// Add event listener to park search button.
+// It will capture user input and place it into userInput variable to be used in the fetch address
 
-const address = `https://data.nashville.gov/resource/xbru-cfzi.json?${userInputParks}=Yes&$$app_token=5twx5LXsJSU43JWk9L2RZ1g5H`;
+const parkSearchButton = document.querySelector("#parkSearch");
 
-fetch(address)
-    .then(parks => parks.json())
-    .then(parsedParks => {
-        
-        parsedParks.forEach(park => {
-            parkName = park.park_name;
-            parkAddress = park.mapped_location_address;
-            let parkHTML = parkBuilder(park);
-            parkAdder(parkHTML);
+parkSearchButton.addEventListener("click", (event) => {
+
+    // Capture user input and place it into the address to be fetched
+
+    userInputParks = document.querySelector("#parksByFeature").value
+
+    
+
+
+    address = `https://data.nashville.gov/resource/xbru-cfzi.json?${userInputParks}=Yes&$$app_token=5twx5LXsJSU43JWk9L2RZ1g5H`;
+    
+   
+    // Fetch all parks data that matches user input
+
+    fetch(address)
+        .then(parks => parks.json())
+        .then(parsedParks => {
+            
+            parsedParks.forEach(park => {
+                parkName = park.park_name;
+                parkAddress = park.mapped_location_address;
+                let parkHTML = parkBuilder(park);
+                parkAdder(parkHTML);
+            })
         })
-    })
-    
-    
-    // Function to build HTML representation of park
-    
-    const parkBuilder = (park) => {
-        return `
-        <div class="park result">
-        <p><strong>${parkName}</strong>: ${parkAddress}</p>
-        <button class="save">Save</button>
-        </div>
-        `
-    }
-    
-    // Function to add HTML to DOM
-    
-    const parksEl = document.querySelector("#parkResults"); 
-    
-    const parkAdder = (parkHTML) => {
-        parksEl.innerHTML += parkHTML;
-    }
+});
 
+    
+// Function to build HTML representation of park
+
+const parkBuilder = (park) => {
+    return `
+    <div class="park result">
+    <p><strong>${parkName}</strong>: ${parkAddress}</p>
+    <button class="save">Save</button>
+    </div>
+    `
+}
+
+
+// Function to add HTML to DOM
+
+const parksEl = document.querySelector("#parkResults"); 
+
+const parkAdder = (parkHTML) => {
+    parksEl.innerHTML += parkHTML;
+}
 
 
 
