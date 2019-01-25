1991269bfd9c06a05d18d3601d744f64f701d2d8
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-01-25 11:01:34 -0600

    working copy

--- scripts/meetups.js
+++ scripts/meetups.js
@@ -1,8 +1,32 @@
-fetch(`https://www.eventbriteapi.com/v3/events/search/`, {
+//this will take in user input
+let eventSearch = "bear"
+//fetch with string interpolation of user input 
+fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${eventSearch}&location.address=nashville&token=CZNRMRCYSJLQZ7VBULG2`, {
     headers: {
         "Authorization": "Bearer CZNRMRCYSJLQZ7VBULG2",
-        "Content-Type": "application/json"
+        "Accept": "application/json"
       },
 })
     .then(response => response.json())
-    .then(entries => {console.log(entries)})
+    .then(entries => {
+        console.log(entries)
+       for (let i = 0; i < entries.events.length; i++) {
+           let eventName = entries.events[i].name.text
+           
+           let eventDescription = entries.events[i].description.text
+
+           let eventLocation =;
+
+           let eventStartDTG = entries.events[i].start.local
+
+           let eventEndDTG = entries.events[i].end.local
+        console.log(eventDateTime)
+       }
+    }) 
+    
+    // function loopEntries (entry){       
+            
+    //         console.log(meet.topic)
+    //     }
+    // }
+    // loopEntries(entries) 
