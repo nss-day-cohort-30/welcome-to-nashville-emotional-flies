9b4458b3a8a3a0c9e349ed4f914059450e3956f2
Kirren Covey <kirrencovey@gmail.com>
2019-01-25 15:34:53 -0600

    user input now gets formatted properly for use in fetch

--- scripts/parks.js
+++ scripts/parks.js
@@ -7,7 +7,7 @@ parkSearchButton.addEventListener("click", (event) => {
 
     // Capture user input and place it into the address to be fetched
 
-    let userInputParks = document.querySelector("#parksByFeature").value
+    let userInputParks = document.querySelector("#parksByFeature").value.toLowerCase().replace(" ", "_");
 
     let address = `https://data.nashville.gov/resource/xbru-cfzi.json?${userInputParks}=Yes&$$app_token=5twx5LXsJSU43JWk9L2RZ1g5H`;
     
