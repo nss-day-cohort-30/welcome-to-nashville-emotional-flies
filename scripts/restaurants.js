80d9db3971a2271e94db239f84c70f43aa2e42e8
Abbey Brown <abbeybrownsemail@gmail.com>
2019-01-28 09:57:54 -0600

    code to ensure first character is capitalized for API search

--- scripts/restaurants.js
+++ scripts/restaurants.js
@@ -5,32 +5,35 @@ const restaurantSearchButton = document.querySelector("#restaurantSearch")
 restaurantSearchButton.addEventListener("click", (event) => {
     //search variable takes the value of the input field 
     let search = document.querySelector("#restaurantsByFoodType").value
-    
+
+    search = search.charAt(0).toUpperCase() + search.slice(1)//capitalizes the first character of the search to ensure user input enters into the API search correctly
+
     if (search === "") {
         alert("Please enter valid restaurant")
     }
+
     //had to put fetch inside of the function, otherwise it wouldn't know
     //where to put the value of the input
     fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${search}&apikey=f3c493d118f4b2a20a5298e22cb4f499`)
-    .then(restaurants => restaurants.json())
-    .then(parsedRestaurants => {
-        for (let i = 0; i < parsedRestaurants.restaurants.length; i++) {
-        const currentRestaurant = parsedRestaurants.restaurants[i];
-        //takes objects and turns them into variables
-        currentRestaurant.name = currentRestaurant.restaurant.name
-        currentRestaurant.address = currentRestaurant.restaurant.location.address
-        currentRestaurant.cuisine = currentRestaurant.restaurant.cuisines
-        currentRestaurant.rating = currentRestaurant.restaurant.user_rating.aggregate_rating
-        //if the currentRestaurant has a cuisine object that is included in the user input's search THEN 
-            if (currentRestaurant.cuisine.includes(search)) {
-                //creates a variable that stores the HTML DOM Representation Function and takes the 
-                //object currentRestaurant as an argument
-            const restaurantHTML = restaurantList(currentRestaurant)
-            //calls on the function printRestaurants with the arugment of restaurantHTML
-            printRestaurants(restaurantHTML)
+        .then(restaurants => restaurants.json())
+        .then(parsedRestaurants => {
+            for (let i = 0; i < parsedRestaurants.restaurants.length; i++) {
+                const currentRestaurant = parsedRestaurants.restaurants[i];
+                //takes objects and turns them into variables
+                currentRestaurant.name = currentRestaurant.restaurant.name
+                currentRestaurant.address = currentRestaurant.restaurant.location.address
+                currentRestaurant.cuisine = currentRestaurant.restaurant.cuisines
+                currentRestaurant.rating = currentRestaurant.restaurant.user_rating.aggregate_rating
+                //if the currentRestaurant has a cuisine object that is included in the user input's search THEN 
+                if (currentRestaurant.cuisine.includes(search)) {
+                    //creates a variable that stores the HTML DOM Representation Function and takes the 
+                    //object currentRestaurant as an argument
+                    const restaurantHTML = restaurantList(currentRestaurant)
+                    //calls on the function printRestaurants with the arugment of restaurantHTML
+                    printRestaurants(restaurantHTML)
+                }
             }
-        }
-    })
+        })
 })
 
 
