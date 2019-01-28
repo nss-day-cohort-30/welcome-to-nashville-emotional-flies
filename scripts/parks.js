64483f537eb713aa7ce097b46e60cb4b293353d0
Kirren Covey <kirrencovey@gmail.com>
2019-01-28 09:46:49 -0600

    working save buttons (adds park name to itinerary and removes park search results

--- scripts/parks.js
+++ scripts/parks.js
@@ -49,4 +49,64 @@ const parkAdder = (parkHTML) => {
 }
 
 
+// Add event listener to save buttons
+// When clicked, it will send the park name to the itinerary and remove the parks results from the page
 
+const itineraryEl = document.querySelector("#itinerary");
+
+
+parksEl.addEventListener("click", () => {
+    
+    // Check that item clicked is a save button
+
+    if (event.target.className === "save") {
+    
+        let buttonClicked = event.target;
+        let parkToSave = buttonClicked.previousElementSibling.textContent.split(":")[0];
+        
+        // Function to build HTML representation of new itinerary item
+        const parkItineraryBuilder = (parkToSave) => {
+            return `
+            <div class="itineraryItem">Park: ${parkToSave}</div>
+            `
+        }
+
+        // Function to add new div to itinerary list
+        const parkItineraryAdder = (parkHTML) => {
+            itineraryEl.innerHTML += parkHTML;
+        }
+        
+        let parkItineraryHTML = parkItineraryBuilder(parkToSave);
+        parkItineraryAdder(parkItineraryHTML);
+
+        console.log(parkToSave)
+
+        // Remove park search results
+        parksEl.innerHTML = "";
+
+    }
+
+});
+
+
+
+
+
+
+
+// Function to build HTML representation of new itinerary item   **** NOT WORKING when in global scope
+
+// const parkItineraryBuilder = (park) => {
+//     return `
+//     <div class="itineraryItem">Park: ${parkToSave}</div>
+//     `
+// }
+
+
+// Function to add new div to itinerary list
+
+// const itineraryEl = document.querySelector("#itinerary");
+
+// const parkItineraryAdder = (parkHTML) => {
+//     itineraryEl.innerHTML += parkHTML;
+// }
